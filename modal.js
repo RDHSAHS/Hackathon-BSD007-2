169c17c4a9fa826c2a02a5d3ae783a3797e8332e
dwiteguh <teguhdwi@gmail.com>
2023-09-01 08:50:10 +0700

    teguh

---
+++ modal.js
@@ -0,0 +1,10 @@
+function username(user){
+    
+    // console.log('helo');
+    let users = ['antan','teguh','akbar','jeremi','dwika']
+
+    for (let i = 0; i < users.length; i++) {
+        console.log(users[i]);
+    }
+}
+console.log(username('user'))
